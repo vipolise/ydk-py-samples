846c392ae79ccaf34ecbed28af919115a514dec1
Santiago Alvarez <saalvare@cisco.com>
2016-06-02 20:51:41 -0700

    Add sample apps for time zone configuration
    Samples include four boilerplate apps for CRUD operations, plus five
    custom apps to create and delete time zone configuration using the XR
    native model (Cisco-IOS-XR-infra-infra-clock-linux-cfg):
    nc-create-config-infra-infra-clock-linux-20-ydk.py - PST PST8PDT
    nc-create-config-infra-infra-clock-linux-22-ydk.py - CST PRC
    nc-create-config-infra-infra-clock-linux-24-ydk.py - CET CET
    nc-create-config-infra-infra-clock-linux-26-ydk.py - BRT Brazil/East
    nc-create-config-infra-infra-clock-linux-28-ydk.py - WAT Africa/Douala
    nc-delete-config-infra-infra-clock-linux-20-ydk.py - delete time zone

---
+++ samples/basic/ydk/models/infra/nc-create-config-infra-infra-clock-linux-24-ydk.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Cisco Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Create config for model Cisco-IOS-XR-infra-infra-clock-linux-cfg.
+
+usage: nc-create-config-infra-infra-clock-linux-24-ydk.py [-h] [-v] device
+
+positional arguments:
+  device         NETCONF device (ssh://user:password@host:port)
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -v, --verbose  print debugging messages
+"""
+
+from argparse import ArgumentParser
+from urlparse import urlparse
+
+from ydk.services import CRUDService
+from ydk.providers import NetconfServiceProvider
+from ydk.models.infra import Cisco_IOS_XR_infra_infra_clock_linux_cfg \
+    as xr_infra_infra_clock_linux_cfg
+import logging
+
+
+def config_clock(clock):
+    """Add config data to clock object."""
+    # time zone configuration
+    time_zone = clock.TimeZone()
+    time_zone.time_zone_name = "CET"
+    time_zone.area_name = "CET"
+    clock.time_zone = time_zone
+
+
+if __name__ == "__main__":
+    """Execute main program."""
+    parser = ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="print debugging messages",
+                        action="store_true")
+    parser.add_argument("device",
+                        help="NETCONF device (ssh://user:password@host:port)")
+    args = parser.parse_args()
+    device = urlparse(args.device)
+
+    # log debug messages if verbose argument specified
+    if args.verbose:
+        logger = logging.getLogger("ydk")
+        logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(("%(asctime)s - %(name)s - "
+                                      "%(levelname)s - %(message)s"))
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    # create NETCONF provider
+    provider = NetconfServiceProvider(address=device.hostname,
+                                      port=device.port,
+                                      username=device.username,
+                                      password=device.password,
+                                      protocol=device.scheme)
+    # create CRUD service
+    crud = CRUDService()
+
+    clock = xr_infra_infra_clock_linux_cfg.Clock()  # create config object
+    config_clock(clock)  # add object configuration
+
+    crud.create(provider, clock)  # create object on NETCONF device
+    provider.close()
+    exit()
+# End of script
---
+++ samples/basic/ydk/models/infra/nc-delete-config-infra-infra-clock-linux-20-ydk.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Cisco Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Delete all config data for model Cisco-IOS-XR-infra-infra-clock-linux-cfg.
+
+usage: nc-delete-config-infra-infra-clock-linux-20-ydk.py [-h] [-v] device
+
+positional arguments:
+  device         NETCONF device (ssh://user:password@host:port)
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -v, --verbose  print debugging messages
+"""
+
+from argparse import ArgumentParser
+from urlparse import urlparse
+
+from ydk.services import CRUDService
+from ydk.providers import NetconfServiceProvider
+from ydk.models.infra import Cisco_IOS_XR_infra_infra_clock_linux_cfg as xr_infra_infra_clock_linux_cfg
+import logging
+
+
+if __name__ == "__main__":
+    """Execute main program."""
+    parser = ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="print debugging messages",
+                        action="store_true")
+    parser.add_argument("device",
+                        help="NETCONF device (ssh://user:password@host:port)")
+    args = parser.parse_args()
+    device = urlparse(args.device)
+
+    # log debug messages if verbose argument specified
+    if args.verbose:
+        logger = logging.getLogger("ydk")
+        logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(("%(asctime)s - %(name)s - "
+                                      "%(levelname)s - %(message)s"))
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    # create NETCONF provider
+    provider = NetconfServiceProvider(address=device.hostname,
+                                      port=device.port,
+                                      username=device.username,
+                                      password=device.password,
+                                      protocol=device.scheme)
+    # create CRUD service
+    crud = CRUDService()
+
+    clock = xr_infra_infra_clock_linux_cfg.Clock()  # create config object
+    crud.delete(provider, clock)  # delete object on NETCONF device
+    provider.close()
+    exit()
+# End of script
