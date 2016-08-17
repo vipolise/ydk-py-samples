2b7d2489ba7e23b90914bd6cdab641a90df67f5b
Santiago Alvarez <saalvare@cisco.com>
2016-08-17 10:09:10 -0700

    Add sample apps for encoding XR BGP config
    Includes a boilerplate app and six custom apps for performing XML
    encoding of XR BGP configuration:
    cd-encode-config-ipv4-bgp-40-ydk.py - iBGP IPv4 neighbor grp w/o policy
    cd-encode-config-ipv4-bgp-41-ydk.py - iBGP IPv6 neighbor grp w/o policy
    cd-encode-config-ipv4-bgp-42-ydk.py - iBGP IPv4 neighbor grp w/ policy
    cd-encode-config-ipv4-bgp-43-ydk.py - iBGP IPv6 neighbor group w/ policy
    cd-encode-config-ipv4-bgp-44-ydk.py - eBGP IPv4 neighbor grp w/ policy
    cd-encode-config-ipv4-bgp-45-ydk.py - eBGP IPv6 neighbor grp w/ policy

---
+++ samples/basic/codec/ydk/models/ipv4/cd-encode-config-ipv4-bgp-44-ydk.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Cisco Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Encode config for model Cisco-IOS-XR-ipv4-bgp-cfg.
+
+usage: cd-encode-config-ipv4-bgp-44-ydk.py [-h] [-v]
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -v, --verbose  print debugging messages
+"""
+
+from argparse import ArgumentParser
+from urlparse import urlparse
+
+from ydk.services import CodecService
+from ydk.providers import CodecServiceProvider
+from ydk.models.ipv4 import Cisco_IOS_XR_ipv4_bgp_cfg as xr_ipv4_bgp_cfg
+from ydk.models.ipv4.Cisco_IOS_XR_ipv4_bgp_datatypes \
+    import BgpAddressFamilyEnum
+from ydk.types import Empty
+import logging
+
+
+def config_bgp(bgp):
+    """Add config data to bgp object."""
+    # global configuration
+    instance = bgp.Instance()
+    instance.instance_name = "default"
+    instance_as = instance.InstanceAs()
+    instance_as.as_ = 0
+    four_byte_as = instance_as.FourByteAs()
+    four_byte_as.as_ = 65001
+    four_byte_as.bgp_running = Empty()
+    # global address family
+    global_af = four_byte_as.default_vrf.global_.global_afs.GlobalAf()
+    global_af.af_name = BgpAddressFamilyEnum.IPV4_UNICAST
+    global_af.enable = Empty()
+    four_byte_as.default_vrf.global_.global_afs.global_af.append(global_af)
+    instance_as.four_byte_as.append(four_byte_as)
+    instance.instance_as.append(instance_as)
+    bgp.instance.append(instance)
+
+    # configure IBGP neighbor group
+    neighbor_groups = four_byte_as.default_vrf.bgp_entity.neighbor_groups
+    neighbor_group = neighbor_groups.NeighborGroup()
+    neighbor_group.neighbor_group_name = "EBGP"
+    neighbor_group.create = Empty()
+    # remote AS
+    neighbor_group.remote_as.as_xx = 0
+    neighbor_group.remote_as.as_yy = 65002
+    neighbor_groups.neighbor_group.append(neighbor_group)
+    # ipv4-unicast address family
+    neighbor_group_af = neighbor_group.neighbor_group_afs.NeighborGroupAf()
+    neighbor_group_af.af_name = BgpAddressFamilyEnum.IPV4_UNICAST
+    neighbor_group_af.activate = Empty()
+    neighbor_group_af.route_policy_in = "POLICY3"  # must be pre-configured
+    neighbor_group_af.route_policy_out = "POLICY1"  # must be pre-configured
+    neighbor_group_afs = neighbor_group.neighbor_group_afs
+    neighbor_group_afs.neighbor_group_af.append(neighbor_group_af)
+
+    # configure IBGP neighbor
+    neighbor = four_byte_as.default_vrf.bgp_entity.neighbors.Neighbor()
+    neighbor.neighbor_address = "192.168.1.1"
+    neighbor.neighbor_group_add_member = "EBGP"
+    four_byte_as.default_vrf.bgp_entity.neighbors.neighbor.append(neighbor)
+
+
+if __name__ == "__main__":
+    """Execute main program."""
+    parser = ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="print debugging messages",
+                        action="store_true")
+    args = parser.parse_args()
+
+    # log debug messages if verbose argument specified
+    if args.verbose:
+        logger = logging.getLogger("ydk")
+        logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(("%(asctime)s - %(name)s - "
+                                      "%(levelname)s - %(message)s"))
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    # create codec provider
+    provider = CodecServiceProvider(type="xml")
+
+    # create codec service
+    codec = CodecService()
+
+    bgp = xr_ipv4_bgp_cfg.Bgp()  # create config object
+    config_bgp(bgp)  # add object configuration
+
+    print(codec.encode(provider, bgp))  # encode and print object
+    provider.close()
+    exit()
+# End of script
